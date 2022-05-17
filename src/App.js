e26f6281534cca0fb85be822c4aab57287d4375b
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2022-05-17 20:05:47 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numbers: 1
+        };
+    }
+
+    render() {
+
+        const { numbers } = this.state;
+        const test = [1 + ' ', + 10, 3 + ' ', 5 + ' '];
+
+        return (
+            <div className="App" >
+                <header className="App-header">
+                    <h1>{test}</h1>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+                {console.log(numbers)}
+
+            </div >
+        );
+    }
 }
+
 
 export default App;
